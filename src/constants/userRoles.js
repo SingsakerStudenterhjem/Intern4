8dfe2ce8b0018322aa9335f887a76171699437a6
Eskild Smestu <esmestu@icloud.com>
2025-07-16 01:11:09 +0200

    Simplify user roles

--- src/constants/userRoles.js
+++ src/constants/userRoles.js
@@ -3,32 +3,3 @@ export const USER_ROLES = {
   REGISJEF: "regisjef",
   USER: "user",
 };
-
-export const PERMISSIONS = {
-  [USER_ROLES.DATA]: [
-    "read_all",
-    "write_all",
-    "manage_users",
-    "manage_system_settings",
-    "view_audit_logs",
-  ],
-  [USER_ROLES.REGISJEF]: [
-    "read_work_entries",
-    "write_work_entries",
-    "approve_work_entries",
-    "read_tasks",
-    "write_tasks",
-    "manage_tasks",
-    "read_users",
-  ],
-  [USER_ROLES.USER]: [
-    "read_own_work_entries",
-    "write_own_work_entries",
-    "read_tasks",
-    "reserve_tasks",
-  ],
-};
-
-export const hasPermission = (userRole, permission) => {
-  return PERMISSIONS[userRole]?.includes(permission) || false;
-};
