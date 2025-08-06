1846b5488dfd30b2c47608827b675af96945d21a
Tmpecho <TMPEcho@gmail.com>
2025-08-06 21:16:36 +0200

    Basic regi page styling

--- src/pages/regi/regiPage.jsx
+++ src/pages/regi/regiPage.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 const RegiPage = () => {
   return (
-    <div>
-      <div>
-        <h1>Regi</h1>
-        <div>
-          <p>Her kan du registrere arbeid du har utført.</p>
+    <div className="flex justify-center min-h-screen bg-gray-100 overflow-x-hidden">
+      <div className="bg-white p-8 rounded-md shadow-md w-full mx-4 my-4 space-y-2 max-w-[80rem] min-w-0">
+        <div className="space-y-1">
+          <h1 className="font-bold text-2xl">Min regi</h1>
+          <p className="text-gray-600 text-sm">Her kan du registrere arbeid du har utført.</p>
         </div>
+
+        {/* Log regi form */}
+        <form></form>
       </div>
     </div>
   );
