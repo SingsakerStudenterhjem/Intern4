d5b8a44ba6e5066bc79398e9be9eeb9e298277a0
Tmpecho <TMPEcho@gmail.com>
2026-05-07 00:36:24 +0200

    Derive navigation from feature manifests

--- src/features/navigation.ts
+++ src/features/navigation.ts
@@ -1,23 +1,6 @@
-import { alcoholNavigation } from './alcohol/navigation';
-import { helgaNavigation } from './helga/navigation';
-import { receptionNavigation } from './reception/navigation';
-import { regiNavigation } from './regi/navigation';
-import { residentNavigation } from './residents/navigation';
-import { shiftNavigation } from './shifts/navigation';
-import { taskNavigation } from './tasks/navigation';
-import { userNavigation } from './users/navigation';
-import { vervNavigation } from './verv/navigation';
-import { wineCellarNavigation } from './wine-cellar/navigation';
+import { features } from '.';
+import type { FeatureNavItem } from '../shared/types/feature';
 
-export const featureNavigation = [
-  ...taskNavigation,
-  ...residentNavigation,
-  ...shiftNavigation,
-  ...regiNavigation,
-  ...vervNavigation,
-  ...alcoholNavigation,
-  ...wineCellarNavigation,
-  ...helgaNavigation,
-  ...userNavigation,
-  ...receptionNavigation,
-];
+export const featureNavigation: FeatureNavItem[] = features.flatMap(
+  (feature) => feature.navigation ?? []
+);
